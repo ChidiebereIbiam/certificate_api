ce28c6ff5e0c7bee39ba52feb3d96390741e1501
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-02-26 12:51:05 +0100

    Created Certificate Model and Serializer Class for Cerificate Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import random
+import qrcode
+from PIL import Image, ImageDraw
+from io import BytesIO
+from django.core.files import File
 
-# Create your models here.
+class Certificate(models.Model):
+    profile_pic = models.ImageField(null=True, blank = True, upload_to = "certificate/")
+    certificate_number = models.CharField(unique=True, max_length=50)
+    fullname = models.CharField(max_length=225)
+    role = models.CharField(max_length=225)
+    organization = models.CharField(default="Kartexa", max_length=255)
+    internship_organizer = models.CharField(max_length=225)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
+    issue_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    verification_link= models.URLField(max_length=200, blank=True, null=True)
+    qr_image = models.ImageField(upload_to='qrcode', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        qrcode_img = qrcode.make(self.verification_link)
+        canvas = Image.new("RGB", (400,400), "white")
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        buffer = BytesIO()
+        canvas.save(buffer, "PNG")
+        self.qr_image.save(f'image{random.randint(0,9999)}.png', File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.fullname} | {self.certificate_number} "
+    
+
---
+++ api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Certificate
+
+class CertificateSerializer(ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = "__all__"
