af630af6735c3f53b4bd2274d46e2545a5523fcf
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-02-26 12:36:13 +0100

    Configured API URLS and added endpoint view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
+def endpoints(request):
+    """Returns all the endpoints available for the api"""
+    data = ['/certificate', 'certificate/:certificate_number', '/verify/:certificate_number']
+    return Response(data)
